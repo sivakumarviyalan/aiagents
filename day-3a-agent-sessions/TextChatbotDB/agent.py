2f3fdaa3364984ef167909cfe12a1a89a03d5ac3
sivakumarviyalan <v.sivakumar.mca@gmail.com>
2025-11-21 14:34:28 -0600

    Add initial implementation of text chatbot with persistent and in-memory session support

---
+++ day-3a-agent-sessions/TextChatbotDB/agent.py
@@ -0,0 +1,138 @@
+from typing import Any, Dict
+
+from google.adk.agents import Agent, LlmAgent
+from google.adk.apps.app import App, EventsCompactionConfig
+from google.adk.models.google_llm import Gemini
+from google.adk.sessions import DatabaseSessionService
+from google.adk.sessions import InMemorySessionService
+from google.adk.runners import Runner
+from google.adk.tools.tool_context import ToolContext
+from google.genai import types
+
+import asyncio
+
+# Load .env
+# Replace the API_KEY in .env file.
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print("✅ ADK components imported successfully.")
+
+retry_config = types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504],  # Retry on these HTTP errors
+)
+
+APP_NAME = "default"  # Application
+USER_ID = "default"  # User
+SESSION = "default"  # Session
+
+MODEL_NAME = "gemini-2.5-flash-lite"
+
+# Step 1: Create the same agent (notice we use LlmAgent this time)
+chatbot_agent = LlmAgent(
+    model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+    name="text_chat_bot",
+    description="A text chatbot with persistent memory",
+)
+
+# Step 2: Switch to DatabaseSessionService
+# SQLite database will be created automatically
+db_url = "sqlite+aiosqlite:///my_agent_data.db"  # Local SQLite file
+session_service = DatabaseSessionService(db_url=db_url)
+
+# Step 3: Create a new runner with persistent storage
+runner = Runner(agent=chatbot_agent, app_name=APP_NAME, session_service=session_service)
+
+print("✅ Upgraded to persistent sessions!")
+print(f"   - Database: my_agent_data.db")
+print(f"   - Sessions will survive restarts!")
+
+# Define helper functions that will be reused throughout the notebook
+async def run_session(
+    runner_instance: Runner,
+    user_queries: list[str] | str = None,
+    session_name: str = "default",
+):
+    print(f"\n ### Session: {session_name}")
+
+    # Get app name from the Runner
+    app_name = runner_instance.app_name
+
+    # Attempt to create a new session or retrieve an existing one
+    try:
+        session = await session_service.create_session(
+            app_name=app_name, user_id=USER_ID, session_id=session_name
+        )
+    except:
+        session = await session_service.get_session(
+            app_name=app_name, user_id=USER_ID, session_id=session_name
+        )
+
+    # Process queries if provided
+    if user_queries:
+        # Convert single query to list for uniform processing
+        if type(user_queries) == str:
+            user_queries = [user_queries]
+
+        # Process each query in the list sequentially
+        for query in user_queries:
+            print(f"\nUser > {query}")
+
+            # Convert the query string to the ADK Content format
+            query = types.Content(role="user", parts=[types.Part(text=query)])
+
+            # Stream the agent's response asynchronously
+            async for event in runner_instance.run_async(
+                user_id=USER_ID, session_id=session.id, new_message=query
+            ):
+                # Check if the event contains valid content
+                if event.content and event.content.parts:
+                    # Filter out empty or "None" responses before printing
+                    if (
+                        event.content.parts[0].text != "None"
+                        and event.content.parts[0].text
+                    ):
+                        print(f"{MODEL_NAME} > ", event.content.parts[0].text)
+    else:
+        print("No queries!")
+
+
+print("✅ Helper functions defined.")
+
+async def main():
+    # await run_session(
+    #     runner,
+    #     ["Hi, I am Sam! What is the capital of the United States?", "Hello! What is my name?"],
+    #     "test-db-session-01",
+    # )
+    # await run_session(
+    #     runner,
+    #     ["What is the capital of India?", "Hello! What is my name?"],
+    #     "test-db-session-01",
+    # )
+    await run_session(
+        runner, ["Hello! What is my name?"], "test-db-session-02"
+    )  # Note, we are using new session name
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+import sqlite3
+
+def check_data_in_db():
+    with sqlite3.connect("my_agent_data.db") as connection:
+        cursor = connection.cursor()
+        result = cursor.execute(
+            "select app_name, session_id, author, content from events"
+        )
+        print([_[0] for _ in result.description])
+        for each in result.fetchall():
+            print(each)
+
+
+check_data_in_db()
